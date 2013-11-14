f6b903214aa9fbfaeb34a1c0e851475849054186
ilteris <ilteris@gmail.com>
2013-11-14 17:16:19 -0500

    install dependencies

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 var request     = require('request');
 var cheerio     = require('cheerio');
+
+
+var ticker  = "LVS";
+var yUrl    = "http://finance.yahoo.com/q/ks?s=" + ticker;
+var financeDetails = new Array();
+var keyStr = new Array();
+request(yUrl, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(body);
+ 
+    // the keys - We get them from a certain class attribute
+    var td = $('.yfnc_tablehead1');
+    $(td).each(function(j, val) {
+      keyStr[j] = $(val).text();
+    });
+ 
+    // the values
+    // TODO: normalize them 
+    var tData = $('.yfnc_tabledata1');
+    $(tData).each(function(j, val) {
+      financeDetails[j] = $(val).text();
+    });
+ 
+    // Let's do something with the data we have
+    for (var i=0; i < financeDetails.length; i++) {
+      console.log (i + ") " + keyStr[i] + " " + financeDetails[i]);
+    }
+
+    }
+}
